db1fe150f20730cbcad9a6390bfa2da20170dba0
jsvajophiel <127799368+jsvajophiel@users.noreply.github.com>
2023-04-06 17:18:26 -0600

    Agregando comentarios
    Se pusieron los comentarios correspondientes para explicar que es lo que se esta realizando en el programa.

--- 001.c
+++ 001.c
@@ -1,10 +1,17 @@
+/* El primer programa en C*/
+/* A continuacion indicamos las bibliotecas a usar */
 #include <stdio.h>
 #include <stdlib.h>
 
+/* Ahora viene el programa principal */
 int main(void)
 {
+    /* La siguiente linea de codigo muestra un mensaje en pantalla */
     printf ("Este es mi primer programa");
+    
+    /* Hacemos una pausa hasta que el usuario pulse Intro */
     printf ("\nPulse Intro para facilitar...");
     getchar();
+    
     return 0;
 }
