b3049a552677fcb7d80fdd2fde3aade739939c40
Julio Vidal <jsvalvarez@yahoo.com.mx>
2023-04-06 17:44:35 -0600

    segundo programa pero en c++

---
+++ 002.cpp
@@ -0,0 +1,15 @@
+* El primer programa en C++ */
+/* A continuacion indicamos las bibliotecas a usar */
+#include <iostream.h>
+
+int main (void)
+{
+    /* La siguiente linea de codigo muestra un mensaje en pantalla */
+    cout << "Este es mi primer programa";
+
+    /* Hacemos una pausa hasta que el usuario pulse Intro */
+    cout << "\nPulse Intro para finalizar...";
+    cin.get();
+
+    return 0;
+}
