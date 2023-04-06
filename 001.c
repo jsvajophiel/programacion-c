ae17ec6681dab972edba0da4a27251628a3937bd
Julio Vidal <jsvalvarez@yahoo.com.mx>
2023-04-06 16:49:21 -0600

    primer archivo de C, pag.56

---
+++ 001.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    printf ("Este es mi primer programa");
+    printf ("\nPulse Intro para facilitar...");
+    getchar();
+
+    return 0;
+}
